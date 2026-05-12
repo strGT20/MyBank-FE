9367a216319b3b97769a0d77227b9a3ca832648a
strGT20 <saptalokazone@gmail.com>
2026-05-12 21:48:29 +0700

    minor adjustment for promo card dan hide saldo

---
+++ app/src/main/java/com/example/mybank/models/PromoList.kt
@@ -0,0 +1,9 @@
+package com.example.mybank.models
+
+data class PromoList(
+    val id: String,          // Penting untuk key di LazyColumn/Row
+    val title: String,
+    val description: String,
+    val hashtag: String,
+    val imageRes: Int        // Saat ini pakai Int (R.drawable), nanti tinggal ganti String (URL)
+)
